1a55bebdabc846fc149720705ca2158c54dfea61
Étienne Lafarge <etienne.lafarge@gmail.com>
2017-09-01 03:52:42 +0200

    Add message forward if recipient is connected
    There's still some work to come to an IRC on top of websockets but we
    have a runnig proof of concept :)

---
+++ broker.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+// Broker represents an abstract broker interface
+type Broker interface {
+	Subscribe(consumer string) (chan []byte, error)
+	HasTopic(consumer string) bool
+	Publish(consumer string, data []byte) error
+	Unsubscribe(consumer string) error
+}
+
+// InMemoryBroker interface implements a simple in-memory Broker using standard Go maps and channels
+type InMemoryBroker struct {
+	channels map[string](chan []byte)
+}
+
+// NewInMemoryBroker returns an in-memory broker instance
+func NewInMemoryBroker() *InMemoryBroker {
+	return &InMemoryBroker{
+		channels: map[string](chan []byte){},
+	}
+}
+
+// HasTopic returns true if the given topic exists, false otherwise
+func (b *InMemoryBroker) HasTopic(consumer string) bool {
+	if _, ok := b.channels[consumer]; ok {
+		return true
+	}
+	return false
+}
+
+// Subscribe returns a channel to messages destinated to a given consumer
+func (b *InMemoryBroker) Subscribe(consumer string) (chan []byte, error) {
+	if !b.HasTopic(consumer) {
+		b.channels[consumer] = make(chan []byte)
+	}
+
+	return b.channels[consumer], nil
+}
+
+// Unsubscribe deregisters a consumer from our broker
+func (b *InMemoryBroker) Unsubscribe(consumer string) error {
+	if !b.HasTopic(consumer) {
+		return fmt.Errorf("Consumer %s doesn't exist", consumer)
+	}
+	delete(b.channels, consumer)
+	return nil
+}
+
+// Publish sends a given payload to the approprate topic (consumer)
+func (b *InMemoryBroker) Publish(consumer string, data []byte) error {
+	if !b.HasTopic(consumer) {
+		return fmt.Errorf("Consumer %s doesn't exist", consumer)
+	}
+	b.channels[consumer] <- data
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+// TODO handle errors on websocket writes
+
 import (
 	"encoding/json"
 	"fmt"
@@ -12,24 +14,45 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Upgrader configuration
-var Upgrader = websocket.Upgrader{
-	ReadBufferSize:  512,
-	WriteBufferSize: 512,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
+var (
+	// CloseErrors is the list of websocket errors we close the connection for
+	CloseErrors = []int{
+		websocket.CloseNormalClosure,
+		websocket.CloseGoingAway,
+		websocket.CloseProtocolError,
+		websocket.CloseUnsupportedData,
+		websocket.CloseNoStatusReceived,
+		websocket.CloseAbnormalClosure,
+		websocket.CloseInvalidFramePayloadData,
+		websocket.ClosePolicyViolation,
+		websocket.CloseMessageTooBig,
+		websocket.CloseMandatoryExtension,
+		websocket.CloseInternalServerErr,
+		websocket.CloseServiceRestart,
+		websocket.CloseTryAgainLater,
+		websocket.CloseTLSHandshake,
+	}
+
+	// Upgrader configuration
+	Upgrader = websocket.Upgrader{
+		ReadBufferSize:  512,
+		WriteBufferSize: 512,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
+)
 
 // Message describes the structure of our "message" payloads
 type Message struct {
+	Sender    string `json:"sender"`
 	Recipient string `json:"recipient"`
-	Message   string `json:"message"`
+	Content   string `json:"content"`
 }
 
 // WebSocketHandler manages bidirectional websocket streams between senders and recipients
 type WebSocketHandler struct {
 	Conf *ChatRelayConf
 
-	writeMutex sync.Mutex
+	broker Broker
 }
 
 func (h WebSocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -41,14 +64,69 @@ func (h WebSocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			Errorln("Error upgrading connection to WebSocket: %v", err)
 		return
 	}
+	defer conn.Close()
 
+	consumer := r.FormValue("user")
+	if len(consumer) == 0 {
+		conn.WriteMessage(
+			websocket.ClosePolicyViolation,
+			[]byte(`{"message": "Your request should contain a "user" parameter"}`),
+		)
+		return
+	}
+
+	// Let's create our Websocket write mutex and wrap in into a send
+	var mut sync.Mutex
+
+	sendWSMessage := func(message []byte) {
+		mut.Lock()
+		err := conn.WriteMessage(websocket.TextMessage, message)
+		mut.Unlock()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"ctx":  "WebSocketHandler",
+				"user": consumer,
+			}).Errorf("Error sending message to browser: %v", err)
+		}
+	}
+
+	// Let's listen for incoming messages from peers in a separate goroutine
+	go func() {
+		msgChan, err := h.broker.Subscribe(consumer)
+		if err != nil {
+			// It's always good to add some contexti (tags) to our logs. Would we push them into a log
+			// parsing solution such as logmatic, splunk, or a Elasticsearch-Fluentd-Kibana stack, this
+			// would enable us to see all logs for a given consumer for instance.
+			log.WithFields(log.Fields{
+				"ctx":  "WebSocketHandler",
+				"user": consumer,
+			}).Errorf("Error listening to incoming messages: %v", err)
+		}
+
+		for {
+			incoming := <-msgChan
+			sendWSMessage(incoming)
+		}
+	}()
+
+	// Let's listen for new message in the goroutine processing the request
 	for {
 		// Note: we're not using conn.ReadMessage() here since it calls ioutil.ReadAll() and makes DDoS
 		// attacks easy: sending an infinite message will overload the RAM.
 		_, reader, err := conn.NextReader()
 		if err != nil {
-			log.WithField("ctx", "ReadMessage").Errorf("Error getting next message reader: %v", err)
-			continue
+			if !websocket.IsCloseError(err, CloseErrors...) {
+				log.WithFields(log.Fields{
+					"ctx":  "WebSocketHandler",
+					"user": consumer,
+				}).Errorf("Error getting next message reader: %v", err)
+				continue
+			}
+			log.WithFields(log.Fields{
+				"ctx":  "WebSocketHandler",
+				"user": consumer,
+			}).Debugf("Close error received: %v", err)
+			break
 		}
 
 		// Read until EOF or abort if buffer is full
@@ -75,41 +153,59 @@ func (h WebSocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 		if tooBig {
-			log.WithField("ctx", "ReadMessage").Debugf("Message is too big, dropping it")
-			h.writeMutex.Lock()
-			conn.WriteMessage(websocket.TextMessage, []byte(`{"message": "too_big"}`))
-			h.writeMutex.Unlock()
+			log.WithFields(log.Fields{
+				"ctx":  "WebSocketHandler",
+				"user": consumer,
+			}).Debugf("Message is too big, dropping it")
+			sendWSMessage([]byte(`{"message": "too_big"}`))
 			continue
 		}
 
 		if readErr != nil {
-			log.WithField("ctx", "ReadMessage").Debugf("Error reading message: %v", readErr)
-			h.writeMutex.Lock()
-			conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("{\"message\": \"read_error\": \"%v\"}", readErr)))
-			h.writeMutex.Unlock()
+			log.WithFields(log.Fields{
+				"ctx":  "WebSocketHandler",
+				"user": consumer,
+			}).Debugf("Error reading message: %v", readErr)
+
+			sendWSMessage([]byte(fmt.Sprintf("{\"message\": \"read_error\": \"%v\"}", readErr)))
 			continue
 		}
 
-		log.WithField("ctx", "ReadMessage").Debugf("Received message %s", string(data))
+		log.WithFields(log.Fields{
+			"ctx":  "WebSocketHandler",
+			"user": consumer,
+		}).Debugf("Received message %s", string(data))
 		var message Message
+		// This Unmarshal call could actually be avoided if we used a "recipient" query parameter.
+		// However, I'm leaving this since, at least to perform some validation, one would probably like
+		// to unmarshal the Payload
 		err = json.Unmarshal(data, &message)
 		if err != nil {
-			log.WithField("ctx", "ReadMessage").Errorf("Error unmarshaling message to JSON: %v", err)
+			log.WithFields(log.Fields{
+				"ctx":  "WebSocketHandler",
+				"user": consumer,
+			}).Errorf("Error unmarshaling message to JSON: %v", err)
 		}
 
-		// if b.HasTopic(message.Recipient) {
-		// 	b.Send(data)
-		// } else {
-		// 	conn.WriteMessage(websocket.TextMessage, []byte(`{"message": "not_found"}`))
-		// }
-		h.writeMutex.Lock()
-		conn.WriteMessage(websocket.TextMessage, []byte(`{"message": "ok"}`))
-		h.writeMutex.Unlock()
+		if !h.broker.HasTopic(message.Recipient) {
+			sendWSMessage([]byte(`{"message": "not_found"}`))
+			continue
+		}
 
+		err = h.broker.Publish(message.Recipient, data)
+		if err != nil {
+			sendWSMessage([]byte(fmt.Sprintf(
+				"{\"message\": \"Error sending message to %s: %v\"}", message.Recipient, err,
+			)))
+		}
+		sendWSMessage([]byte(`{"message": "ok"}`))
 	}
 
-	w.WriteHeader(200)
-	w.Write([]byte(`{"message": "Connection terminated with success"}`))
+	h.broker.Unsubscribe(consumer)
+	log.WithFields(log.Fields{
+		"ctx":  "HTTPHandler",
+		"user": consumer,
+	}).Debugf("Connection terminated with success")
 }
 
 func main() {
@@ -120,6 +216,8 @@ func main() {
 
 	handler := WebSocketHandler{
 		Conf: conf,
+
+		broker: NewInMemoryBroker(),
 	}
 
 	log.WithField("ctx", "server-main").Infoln("Starting gochat websocket server")
